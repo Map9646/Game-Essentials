8204ea6a6cf8fcbb4217769d048797dee9af755a
Megan <petersen.megan9646@gmail.com>
2020-12-16 08:49:48 -0700

    My Game is Done and ready

---
+++ The Bizarre Adventures of Mr. Penguin Prototype/Assets/Scripts/EnemyColorBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyColorBehaviour : MonoBehaviour
+{
+    public Material newMaterial;
+    public Material defaultMaterial;
+
+    private MeshRenderer meshR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshR = GetComponent<MeshRenderer>();
+        meshR.material = defaultMaterial;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        meshR.material = newMaterial;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        meshR.material = defaultMaterial;
+    }
+}
